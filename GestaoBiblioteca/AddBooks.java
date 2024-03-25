646fc9bde4b60638d44fe31554b2e67cbee9fc8c
João Farias <victor_reis_farias@hotmail.com>
2024-03-25 16:40:46 -0300

    editando variaveis e atualizando array

--- GestaoBiblioteca/AddBooks.java
+++ GestaoBiblioteca/AddBooks.java
@@ -4,11 +4,11 @@ public class AddBooks {
     public void adicionarLivro(String titulo, String autor) {
         Livro novoLivro;
         System.out.println("Livro adicionado: " + titulo + " por " + autor);
-        ArrayList<Livro> BDLibary = new ArrayList<>();
-        BDLibary.add(titulo);
-        BDLibary.add(autor);
-        System.out.println("Livro cadastrado: "  + BDLibary.get(0));
-        System.out.println("Livro cadastrado: "  + BDLibary.get(1));
+        ArrayList<String> BDLibrary = new ArrayList<>();
+        BDLibrary.add(titulo);
+        BDLibrary.add(autor);
+        System.out.println("Livro cadastrado: "  + BDLibrary.get(0));
+        System.out.println("Livro cadastrado: "  + BDLibrary.get(1));
         
     }
 }
